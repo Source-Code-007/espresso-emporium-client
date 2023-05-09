8e3aa51999d73f043f528956f5d65d5b711bfbbe
Source-Code-007 <utsho926@gmail.com>
2023-05-09 15:23:59 +0600

    banner added

---
+++ src/Component/Homepage/Coffees/Coffees.jsx
@@ -0,0 +1,13 @@
+const Coffees = () => {
+    return (
+        <div>
+            <div>
+                <p>--- Sip & Savor ---</p>
+                <h2>Our popular products</h2>
+                <button className="cmn-btn">add coffee</button>
+            </div>
+        </div>
+    );
+};
+
+export default Coffees;
